5db06cb411f708ccf9be8723941d128286acef06
Felipe-Euzebio <felipe.euzebio.ti@gmail.com>
2023-03-21 16:56:11 -0300

    First MVC - Department

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 using System.Diagnostics;
 
 namespace SalesWebMvc.Controllers
@@ -15,6 +16,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult Index()
         {
+            ViewData["Page"] = "Dashboard";    
+
             return View();
         }
 
